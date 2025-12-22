366225de8e7f6e352a9675dfcbeb81bcf62fd9a5
Kumara Swamy <Kumarmanikala007@gmail.com>
2025-12-23 00:25:43 +0530

    Implement check for sorted and rotated array

---
+++ BinarySearch/Arrays/1752_CheckSortedAndRotated.java
@@ -0,0 +1,23 @@
+// LeetCode 1752 - Check if Array Is Sorted and Rotated
+// Approach: Count order breaks
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+    public boolean check(int[] nums) {
+        int count = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                count++;
+            }
+        }
+
+        if (nums[n - 1] > nums[0]) {
+            count++;
+        }
+
+        return count <= 1;
+    }
+}
