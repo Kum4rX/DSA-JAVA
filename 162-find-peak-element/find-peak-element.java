051014eaf8e70020fac14b8d74bf6969f26c33d7
Kumara Swamy <Kumarmanikala007@gmail.com>
2026-02-03 00:46:39 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (68.54%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,15 +1,10 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-         int low = 0, high = nums.length - 1;
-         while(low < high) {
-            int mid = (low + high) / 2;
-
-            if(nums[mid] > nums[mid + 1]) {
-                high = mid;
-            } else {
-                low = mid + 1;
+         for (int i = 0; i < nums.length; i++) {
+            if ((i == 0 || nums[i] > nums[i - 1]) && (i == nums.length - 1 || nums[i] > nums[i + 1])) {
+                return i;
             }
         }
-        return low;
+        return -1;
     }
 }
