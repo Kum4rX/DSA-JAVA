7d8b961745a908e88f67952e62500c3efc79c33f
Kumara Swamy <Kumarmanikala007@gmail.com>
2026-02-16 23:17:25 +0530

    Time: 1 ms (60.34%) | Memory: 42.5 MB (52.75%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int reverseBits(int n) {
+        return Integer.reverse(n);
+    }
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.reverseBits(1));
+        System.out.println(s.reverseBits(2));
+        System.out.println(s.reverseBits(3));
+        System.out.println(s.reverseBits(4));
+        System.out.println(s.reverseBits(5));
+        System.out.println(s.reverseBits(6));
+        System.out.println(s.reverseBits(7));
+        System.out.println(s.reverseBits(8));
+        System.out.println(s.reverseBits(9));
+        System.out.println(s.reverseBits(10));
+        System.out.println(s.reverseBits(11));
+        System.out.println(s.reverseBits(12));
+        System.out.println(s.reverseBits(13));
+        System.out.println(s.reverseBits(14));
+        System.out.println(s.reverseBits(15));
+    }
+}
