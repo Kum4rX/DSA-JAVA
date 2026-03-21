bca732d5db81b84bcc500c2b483b4cac2e88d12d
Kumara Swamy <Kumarmanikala007@gmail.com>
2026-03-21 14:51:23 +0530

    Time: 1 ms (58.37%) | Memory: 46.7 MB (99.18%) - LeetSync

--- 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.java
+++ 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.java
@@ -4,9 +4,7 @@ class Solution {
             for (int j = 0; j < k; j++) {
 
                 int temp = grid[x + i][y + j];
-
                 grid[x + i][y + j] = grid[x + k - 1 - i][y + j];
-
                 grid[x + k - 1 - i][y + j] = temp;
             }
         }
